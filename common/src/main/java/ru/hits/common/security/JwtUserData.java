adbeadbaebbe2c6ee1ce0edbf72692bf084b0ff5
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2024-05-30 19:07:49 +0700

    Создан модуль для работы с user

---
+++ common/src/main/java/ru/hits/common/security/JwtUserData.java
@@ -0,0 +1,17 @@
+package ru.hits.common.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class JwtUserData {
+    private final UUID id;
+
+    private final String login;
+
+    private final String name;
+
+}
