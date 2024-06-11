3b42cab7db0091c5565774777dd05bb0dc2db0cf
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2024-06-11 10:13:28 +0700

    добавил модуль для ядра документового сервиса

---
+++ doc-core/src/main/java/ru/hits/DocCore.java
@@ -0,0 +1,11 @@
+package ru.hits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocCore {
+    public static void main(String[] args) {
+        SpringApplication.run(DocCore.class, args);
+    }
+}
